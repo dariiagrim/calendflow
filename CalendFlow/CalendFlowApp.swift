a93201d5120349f385d7bfeb0ec121d87d73ec29
Dariia Moisol <dariia.moisol@howly.com>
2024-05-11 15:33:50 +0300

    Initial Commit

---
+++ CalendFlow/CalendFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendFlowApp.swift
+//  CalendFlow
+//
+//  Created by User on 11.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct CalendFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
